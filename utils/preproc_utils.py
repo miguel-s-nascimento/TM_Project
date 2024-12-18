a5d2a89fe47c73529b8847c115230d7eb8e8b8e5
David Miguel Gomes Duarte <165814185+Notplays@users.noreply.github.com>
2024-12-18 18:42:53 +0000

    sem word correction

--- utils/preproc_utils.py
+++ utils/preproc_utils.py
@@ -4,25 +4,29 @@ import nltk
 from unidecode import unidecode
 from nltk.tokenize.treebank import TreebankWordDetokenizer
 from utils.gibberish_detector import classify_gibberish
+import contractions
 
 
 def regex_cleaner(raw_text, 
             no_emojis = True, 
             no_hashtags = True,
             hashtag_retain_words = True,
-            no_newlines = True,
+            no_newlines_carr_return = True,
             no_urls = True,
-            no_punctuation = True):
+            no_punctuation = True,
+            no_contractions = True,
+            no_numbers = True):
     
     #patterns
-    newline_pattern = "(\\n)"
+    newline_carr_return_pattern = "[\r\n]+"
     hashtags_at_pattern = "([#\@@\u0040\uFF20\uFE6B])"
     hashtags_ats_and_word_pattern = "([#@]\w+)"
     emojis_pattern = "(\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])"
     url_pattern = "(?:https?://)?(?:www\.)?[a-zA-Z0-9.-]+\.(?:com)(?:/[a-zA-Z0-9./?=&_-]*)?"
     punctuation_pattern = "[\u0021-\u0026\u0028-\u0029\u002B\u002C\u002E-\u002F\u003A-\u003F\u005B-\u005F\u2010-\u2028\ufeff`]+"
     apostrophe_pattern = "'(?=[A-Z\s])|(?<=[a-z\.\?\!\,\s])'"
-    separated_words_pattern = "(?<=\w\s)([A-Z]\s){2,}"
+    numbers_pattern = "\d+"
+    
     
     if no_emojis:
         clean_text = re.sub(emojis_pattern,"",raw_text)
@@ -35,27 +39,32 @@ def regex_cleaner(raw_text,
         else:
             clean_text = re.sub(hashtags_ats_and_word_pattern,"",clean_text)
         
-    if no_newlines:
-        clean_text = re.sub(newline_pattern," ",clean_text)
+    if no_newlines_carr_return:
+        clean_text = re.sub(newline_carr_return_pattern," ",clean_text)
 
     if no_urls:
         clean_text = re.sub(url_pattern,"",clean_text)
-    
-    if no_punctuation:
-        clean_text = re.sub(punctuation_pattern,"",clean_text)
-        clean_text = re.sub(apostrophe_pattern,"",clean_text)
 
+    if no_contractions:
+        clean_text = contractions.fix(clean_text) # before apostrophe removal
+
+    if no_punctuation:
+        clean_text = re.sub(punctuation_pattern," ",clean_text)
+        clean_text = re.sub(apostrophe_pattern," ",clean_text)
+        clean_text = re.sub("\s{2,}", " ", clean_text) # Eliminate multiple spaces
+        
+    if no_numbers:
+        clean_text = re.sub(numbers_pattern, "", clean_text)
+    
     return clean_text
 
 def lemmatize_all(token, list_pos=["n","v","a","r","s"]):
-    
     wordnet_lem = nltk.stem.WordNetLemmatizer()
     for arg_1 in list_pos:
         token = wordnet_lem.lemmatize(token, arg_1)
     return token
 
-def main_pipeline(raw_text, 
-                  print_output = True, 
+def main_pipeline(raw_text,  
                   no_stopwords = True,
                   custom_stopwords = [],
                   exception_stopwords = [],
@@ -74,10 +83,6 @@ def main_pipeline(raw_text,
     clean_text = regex_cleaner(raw_text, **kwargs)
 
     tokenized_text = nltk.tokenize.word_tokenize(clean_text)
-
-    tokenized_text = [re.sub("'m","am",token) for token in tokenized_text]
-    tokenized_text = [re.sub("n't","not",token) for token in tokenized_text]
-    tokenized_text = [re.sub("'s","is",token) for token in tokenized_text]
     
     if no_stopwords:
         stopwords = nltk.corpus.stopwords.words("english")
@@ -103,17 +108,12 @@ def main_pipeline(raw_text,
     
     if lowercase:
         tokenized_text = [item.lower() for item in tokenized_text]
-
-    if print_output:
-        print(raw_text)
-        print(tokenized_text)
     
     if pos_tags_list == "pos_list":
         return (tokenized_text, pos_tags)
     elif pos_tags_list == "pos_tuples":
         return pos_tuples   
     if word_correction:
-        print(tokenized_text)
         spell = Speller()
         tokenized_text= [spell(item) for item in tokenized_text]
 
